8074cd84460ab4c329828cff528cfa1b7310a2d3
anas <anaselhaouss89@gmail.com>
2024-12-24 10:55:37 +0100

    fix the issue

--- TIK-TAK-TOE.py
+++ TIK-TAK-TOE.py
@@ -1,37 +1,50 @@
-from abc import *
-
-class Shape(ABC):
-    @abstractmethod
-    def area(self):
-        pass
-
-class Circle(Shape):
-    def __init__(self,r,pi=3.14):
-        self.r = r
-        self.pi = pi
-    def area(self):
-        S = self.r * self.r * self.pi 
-        return S
-class Rectangle(Shape):
-    def __init__(self,a,b):
-        self.a = a
-        self.b = b
-    def area(self):
-        S = self.a * self.b
-        return S
-class Triangle(Shape):
-    def __init__(self,a,b):
-        self.a = a
-        self.b = b
-    def area(self):
-        S = 0.5 * self.a * self.b
-        return S
-    
-circle = Circle(2)
-print(circle.area())
-
-rectangle = Rectangle(3,4)
-print(rectangle.area())
-
-triangle = Triangle(6,2)
-print(triangle.area())
+class board:
+    def __init__(self):
+        self.board=[" " for i in range(1,10)]
+    def wrap(self):
+        for i in range(0,9,3):
+            print(f"{self.board[i]} | {self.board[i+1]} | {self.board[i+2]}")
+            if i<6:
+                print("---------")
+    def placeX(self):
+        placex=0 
+        while not (placex >0 and placex<=9):
+            placex=int(input("gamer1: "))        
+        return placex
+    def placeO(self):
+        placeo=0
+        while not( placeo>0 and placeo<=9):
+            placeo=int(input("gamer2: "))     
+        return placeo
+    def show(self):
+      x=0
+      self.wrap()
+      
+      for i in range(5) :
+            px=self.placeX()
+            while not (self.board[px-1]!="X" and self.board[px-1]!="O"):
+                px=self.placeX()
+            self.board[px-1]="X"
+            self.wrap()
+            if (self.board[0]=="X" and self.board[1]=="X" and self.board[2]=="X") or (self.board[3]=="X" and self.board[4]=="X" and self.board[5]=="X") or (self.board[6]=="X" and self.board[7]=="X" and self.board[8]=="X")or (self.board[0]=="X" and self.board[3]=="X" and self.board[6]=="X") or (self.board[1]=="X" and self.board[4]=="X" and self.board[7]=="X") or (self.board[2]=="X" and self.board[5]=="X" and self.board[8]=="X") or (self.board[0]=="X" and self.board[4]=="X" and self.board[8]=="X") or (self.board[2]=="X" and self.board[4]=="X" and self.board[6]=="X"):
+                print("Gamer 1 you are win") 
+                break
+            x+=1
+            if x==9:
+                print("it's a draw")
+                break
+            po=self.placeO()
+            while not (self.board[po-1]!="X" and self.board[po-1]!="O"):
+              po=self.placeO()
+            self.board[po-1]="O"
+            self.wrap()
+            if (self.board[0]=="O" and self.board[1]=="O" and self.board[2]=="O") or (self.board[3]=="O" and self.board[4]=="O" and self.board[5]=="O") or (self.board[6]=="O" and self.board[7]=="O" and self.board[8]=="O")or (self.board[0]=="O" and self.board[3]=="O" and self.board[6]=="O") or (self.board[1]=="O" and self.board[4]=="O" and self.board[7]=="O") or (self.board[2]=="O" and self.board[5]=="O" and self.board[8]=="O") or (self.board[0]=="O" and self.board[4]=="O" and self.board[8]=="O") or (self.board[2]=="O" and self.board[4]=="O" and self.board[6]=="O"):
+                print("Gamer 2 you are win")
+                break
+            x+=1
+            if x==9 :
+                print("it's a Draw")
+                break
+                        
+game=board()
+game.show()
